db99441ed3cdff4062a8c99118332c9fe6771e8b
Christophe Duchesne <christophe.duchesne.96@gmail.com>
2018-08-14 13:37:38 -0400

    Homepage changed to monitoring

--- pdWebMonitoring/urls.py
+++ pdWebMonitoring/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('monitoring/', include('pdWebMonitoring.monitoring.urls')),
+    path('', include('pdWebMonitoring.monitoring.urls')),
     path('admin/', admin.site.urls),
 ]
